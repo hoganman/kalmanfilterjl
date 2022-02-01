10159fac20cde06b95cc8692a2931fb47ad6a0fd
Matthew Hogan <matthewghogan@pm.me>
2022-01-31 20:50:31 -0700

    Update the state estimate

--- kalmanflt.jl
+++ kalmanflt.jl
@@ -1,14 +1,12 @@
 using LinearAlgebra
 
 
-mutable struct klfilter{T}
+mutable struct klobservation{T}
 
     # Observation matrix
-    observation::Matrix{T}
+    obs::Matrix{T}
     # Uncertainty on observation
-    cov::Matrix{T}
-    # Kalman gain
-    gain::Matrix{T}
+    obsCov::Matrix{T}
 
 end
 
@@ -42,4 +40,31 @@ function extrapulate_state(
     updated_state::Vector = system.transition * state.state + system.control * input
     updated_cov::Matrix = system.transition * state.cov * transpose(system.transition) 
                                 + system.noise * input
+end
+
+function update_state(
+    current_state::klstate,
+    current_filter::klobservation,
+    measurement::Vector)::klstate
+
+    # Intermediate calculation (1)
+    obs_transpose = transpose(current_filter.obs)
+
+    # Compute the Kalman gain
+    gain = (current_state.cov * obs_transpose
+            * (current_filter.obs * current_state.cov * obs_transpose
+               + current_filter.obsCov)^(-1))
+
+    # Intermediate calculations (2)
+    transformation = (ones(eltype(current_state.cov), size(current_state.cov))
+                      - gain * current_filter.obs)
+
+    # Update the state estimate and covariance
+    updated_state_vec = (current_state.state + gain 
+                         * (measurement - current_filter.obs * current_state.state))
+    updated_cov = (transformation * current_state.cov * transpose(transformation)
+                   + gain * current_filter.obsCov * transpose(gain))
+    updated_state = klstate(updated_state_vec, updated_cov)
+    
+    return updated_state
 end
