ba643e016fb1c2a081174cc2c9c25713dd8cc417
Matthew Hogan <matthewghogan@pm.me>
2022-02-01 22:47:08 -0700

    Fix broken gain code

--- constantaccel.jl
+++ constantaccel.jl
@@ -8,10 +8,12 @@ function get_model_estimate(
     measurement_time_sec)::Vector
 
     accel_ms2 = 9.8 # meter / sec / sec
-    model_estimate_vec = ([1.0 measurement_time_sec; 0.0 1.0] * initial_state
-                          - accel_ms2 * ([0.5 * measurement_time_sec^2 0; 0 measurement_time_sec] 
-                          * Vector([1., 1.])))
-    return model_estimate
+    obs_mat = [1.0 measurement_time_sec; 0.0 1.0]
+    external_input_vector = Vector([accel_ms2, accel_ms2])
+    control_mat = [0.5 * measurement_time_sec ^ 2 0; 0 measurement_time_sec]
+    model_estimate_vec = (obs_mat * initial_state
+                          - (control_mat * external_input_vector))
+    return model_estimate_vec
 end
 
 function get_measurement(
@@ -22,17 +24,17 @@ function get_measurement(
 
     model_estimate_vec = get_model_estimate(initial_state, measurement_time_sec)
     # Pseudo-measurement using classical physics calculation with noise
-    position_noise_m = 7.5  # meter
-    velocity_noise_ms = 0.15  # meter / sec
-    noise_vector = Vector([position_noise_m * randn(1); velocity_noise_ms * randn(1)])
-    measurement::Vector = model_estimate_vec + noise_vector
+    position_noise_m = 5.0  # meter
+    velocity_noise_ms = 0.1  # meter / sec
+    artificial_noise_vec = Vector([position_noise_m * randn(1); velocity_noise_ms * randn(1)])
+    measurement::Vector = model_estimate_vec + artificial_noise_vec
     return measurement
 
 end
 
 function constantaccel()
 
-    input_accel_ms2 = 9.8  # meter / sec / sec
+    accel_ms2 = 9.8  # meter / sec / sec
     delta_time_s = 0.1
 
     """Define a system where a falling object is released at rest at 1 km above the
@@ -40,25 +42,37 @@ function constantaccel()
     The altitude variance set to (10.0 meter)^2.
     The velocity variance set to (0.1 meter/sec)^2
     """
-    initial_state_vector = Vector([1000.0, 0.0])
+    initial_state_vector = Vector([1000.0, -0.001])
     initial_state_cov = Diagonal([10.0^2, 0.1^2])
     initial_state = klstate{Float64}(initial_state_vector, initial_state_cov)
 
     """The system is just a particle falling due to gravity without air resistance"""
     state_transition_mat = [1.0 delta_time_s; 0.0 1.0]
-    control_mat = [0.5 * delta_time_s ^ 2 0; 0 delta_time_s]
-    external_input_vector = Vector([input_accel_ms2, input_accel_ms2])
+    control_mat = [0.5 * delta_time_s^2 0; 0 delta_time_s]
     system_noise_mat = zeros(Float64, (2, 2))
+    external_input_vector = -1 * Vector([accel_ms2, accel_ms2])
     system = klsystem{Float64}(state_transition_mat, control_mat, system_noise_mat)
 
-    """Initialize the Kalman filter"""
-    obs_mat = [1.0 measurement_time_sec; 0.0 1.0]
-    measurementCov = Diagonal([10.0^2, 0.1^2])
+    """Initialize the observation matrix. The measurement and state is one-to-one"""
+    obs_mat = Diagonal([1.0, 1.0])
+    obsCov = Diagonal([5.0^2, 0.25^2])
+    observations = klobservation{Float64}(obs_mat, obsCov)
 
-    for step in 1:10
+    next_state = klstate{Float64}(initial_state.state, initial_state.cov)
+    for step in 1:100
+        # At current time, get measurement
         time_s = step * delta_time_s
         measurement_vec = get_measurement(initial_state.state, time_s)
-        println("Time $time_s, measurement $measurement_vec")
+        true_state_vec = get_model_estimate(initial_state.state, time_s)
+        println("Time $time_s, measurement $measurement_vec, true $true_state_vec")
+
+        # Predict the next state
+        predicted_state = extrapulate_state(next_state, system, external_input_vector)
+
+        # Update the estimate
+        next_state = update_state(predicted_state, observations, measurement_vec)
+        println("The Kalman gain state is $(next_state.state) with cov = $(next_state.cov)")
+
     end
 end
 
--- kalmanflt.jl
+++ kalmanflt.jl
@@ -37,13 +37,16 @@ function extrapulate_state(
     input::Vector)::klstate
     
     # Update the state vector
-    updated_state::Vector = system.transition * state.state + system.control * input
+    updated_state_vec::Vector = system.transition * state.state + system.control * input
     updated_cov::Matrix = system.transition * state.cov * transpose(system.transition) 
                                 + system.noise * input
+
+    updated_state = klstate{eltype(updated_state_vec)}(updated_state_vec, updated_cov)
+    return updated_state
 end
 
 function update_state(
-    current_state::klstate,
+    predicted_state::klstate,
     current_filter::klobservation,
     measurement::Vector)::klstate
 
@@ -51,20 +54,19 @@ function update_state(
     obs_transpose = transpose(current_filter.obs)
 
     # Compute the Kalman gain
-    gain = (current_state.cov * obs_transpose
-            * (current_filter.obs * current_state.cov * obs_transpose
+    gain = (predicted_state.cov * obs_transpose
+            * (current_filter.obs * predicted_state.cov * obs_transpose
                + current_filter.obsCov)^(-1))
 
-    # Intermediate calculations (2)
-    transformation = (ones(eltype(current_state.cov), size(current_state.cov))
-                      - gain * current_filter.obs)
+    # Intermediate calculation (2)
+    transformation = (I(size(predicted_state.cov)[1]) - gain * current_filter.obs)
 
     # Update the state estimate and covariance
-    updated_state_vec = (current_state.state + gain 
-                         * (measurement - current_filter.obs * current_state.state))
-    updated_cov = (transformation * current_state.cov * transpose(transformation)
+    updated_state_vec = (predicted_state.state + gain 
+                         * (measurement - current_filter.obs * predicted_state.state))
+    updated_cov = (transformation * predicted_state.cov * transpose(transformation)
                    + gain * current_filter.obsCov * transpose(gain))
-    updated_state = klstate(updated_state_vec, updated_cov)
+    updated_state = klstate{eltype(updated_state_vec)}(updated_state_vec, updated_cov)
     
     return updated_state
 end
