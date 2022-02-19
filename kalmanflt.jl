2ed615718aab6663a44a1854b33d3500389e7200
Matthew Hogan <matthewghogan@pm.me>
2022-02-19 15:36:46 -0700

    Update constant acceleration notebook with more clear methods

--- kalmanflt.jl
+++ kalmanflt.jl
@@ -12,26 +12,36 @@ The Kalman filter observation and covariance matrices
 
 See also [`kfsystem`](@ref), [`kfstate`](@ref)
 """
-mutable struct kfobservation{T}
+mutable struct kfobservation{T <: Real}
+
+    # Measurement vector
+    meas::Vector{T}
     # Observation matrix
     obs::Matrix{T}
     # Uncertainty on observation
     obsCov::Matrix{T}
+
 end
 
-"""
-    kfsystem{T}(transition, control, noise)
+kfobservation{T}(obs::Matrix{T}, obsCov::Matrix{T}) where {T<:Real} = kfobservation{T}(
+    zeros(T, size(obs)[1]), 
+    obs,
+    obsCov
+);
 
-The Kalman filter observation and covariance matrices
+"""
+kfupdate{T}(transition, control, noise)
+
+The Kalman filter transition, control, and noise covariance matrices, collectively the update matrices
 
 # Arguments
--`transition`: State transition `Matrix`
--`control`: Input/external control `Matrix `
--`noise`: Estimated noise covariance `Matrix` of the state transition
+ -`transition`: State transition `Matrix`
+ -`control`: Input/external control `Matrix `
+ -`noise`: Estimated noise covariance `Matrix` of the state transition
 
 See also [`kfobservation`](@ref), [`kfstate`](@ref)
 """
-mutable struct kfsystem{T}
+struct kfupdate{T <: Real}
     # State transition matrix
     transition::Matrix{T}
     # Control matrix
@@ -46,12 +56,12 @@ end
 The Kalman filter state vector and associated covariance matrix
 
 # Arguments
--`state`: State `Vector`
--`cov`: Covariance `Matrix` of the state
+ -`state`: State `Vector`
+ -`cov`: Covariance `Matrix` of the state
 
 See also [`kfobservation`](@ref), [`kfsystem`](@ref)
 """
-mutable struct kfstate{T}
+struct kfstate{T <: Real}
     # State vector
     state::Vector{T}
     # Covariance matrix
@@ -61,53 +71,53 @@ end
 
 
 """
-    extrapulate_state(state, system, input)
+predict_state(state, system, input)
 
-Predict/extrapolate the current state to the next
+Predict the next state vector and covariance
 
 # Arguments
-`state`: [`kfstate`](@ref) instance
-`system`: [`kfsystem`](@ref) instance
-`input`: External input `Vector`
+ -`state`: [`kfstate`](@ref) instance
+ -`update_matrices`: [`kfupdate`](@ref) instance
+ -`input`: External input `Vector`
 
 # Returns
 [`kfstate`](@ref)
 
 See also [`update_state`](@ref)
 """
-function extrapulate_state(
+function predict_state(
     state::kfstate,
-    system::kfsystem,
-    input::Vector)::kfstate
+    update_matrices::kfupdate,
+    external_input::Vector
+)::kfstate
     # Update the state vector
-    updated_state_vec::Vector = (system.transition * state.state 
-                                 + system.control * input)
-    updated_cov::Matrix = ((system.transition * state.cov 
-                           * transpose(system.transition)) + system.noise)
+    updated_state_vec::Vector = (update_matrices.transition * state.state 
+                                 + update_matrices.control * external_input)
+    updated_state_cov::Matrix = ((update_matrices.transition * state.cov 
+                           * transpose(update_matrices.transition)) + update_matrices.noise)
 
-    updated_state = kfstate{eltype(updated_state_vec)}(updated_state_vec, updated_cov)
+    updated_state = kfstate{eltype(updated_state_vec)}(updated_state_vec, updated_state_cov)
     return updated_state
 end
 
 """
-    update_state(predicted_state, current_filter, measurement)
+    correct_state(predicted_state, current_filter, measurement)
 
 Update the current state estimate with a measurement
 
 # Arguments
-`predicted_state`: [`kfstate`](@ref) instance after running [`extrapulate_state`](@ref)
-`observation`: [`kfobservation`](@ref) instance dictating the observation of the system
-`measurement`: The measurement `Vector`
+ -`predicted_state`: [`kfstate`](@ref) instance after running [`extrapulate_state`](@ref)
+ -`observation`: [`kfobservation`](@ref) instance dictating the observation of the system
+ -`measurement`: The measurement `Vector`
 
 # Returns
 [`kfstate`](@ref)
 
 See also [`extrapulate_state`](@ref)
 """
-function update_state(
+function correct_state(
     predicted_state::kfstate,
-    observation::kfobservation,
-    measurement::Vector
+    observation::kfobservation
 )::kfstate
     # Intermediate calculation (1)
     obs_transpose = transpose(observation.obs)
@@ -122,7 +132,7 @@ function update_state(
 
     # Update the state estimate and covariance
     updated_state_vec = (predicted_state.state + gain
-                         * (measurement - observation.obs * predicted_state.state))
+                         * (observation.meas - observation.obs * predicted_state.state))
     updated_cov = (transformation * predicted_state.cov * transpose(transformation)
                    + gain * observation.obsCov * transpose(gain))
     updated_state = kfstate{eltype(updated_state_vec)}(updated_state_vec, updated_cov)
